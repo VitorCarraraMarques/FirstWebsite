e07e505eabb11079f938a8d066967dc8fe04905a
Vítor Carrara <vitorcarrara1998@gmail.com>
2023-02-01 11:07:19 -0300

    Add tags and app for django-crispy style

--- First_Website/First_Website/settings.py
+++ First_Website/First_Website/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
